befd3653df5881274724ca84ca3ab0786bd3ad0b
Omaury Marmolejo <omaury@tango.io>
2023-12-20 13:28:24 -0600

    Add invitation associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
-  has_many :invitations
+  has_many :invitations, dependent: :destroy
   has_many :invited_users, through: :invitations, source: :user
 
   validates :title, :description, :date, :location, presence: true
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,7 @@
 class Invitation < ApplicationRecord
   belongs_to :event
   belongs_to :user
+
+  validates :status, presence: true
+  validates :user_id, uniqueness: { scope: :event_id }
 end
