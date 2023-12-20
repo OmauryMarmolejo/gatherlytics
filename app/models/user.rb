887ec58c33b06cb41576235fd3e5ac607231224e
Omaury Marmolejo <omaury@tango.io>
2023-12-20 14:05:37 -0600

    Remove jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
-  :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   has_many :events, dependent: :destroy
   has_many :invitations, dependent: :destroy
