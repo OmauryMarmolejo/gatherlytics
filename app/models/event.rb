791a0970835796df262fdce900672800eae1dde3
Omaury Marmolejo <omaury@tango.io>
2023-12-20 13:27:38 -0600

    Add user associations and controllers

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :invitations
+  has_many :invited_users, through: :invitations, source: :user
+
+  validates :title, :description, :date, :location, presence: true
 end
